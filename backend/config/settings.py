9c9afdd6fb7873479e2707afd72a9c4f4541122a
Gohel Yash <yashgohel0000@gmail.com>
2026-06-25 12:04:27 +0530

    Ui changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-insecure-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
